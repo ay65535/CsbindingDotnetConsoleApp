257756a2afe0bd217698ac4687072a5fb586b06b
ay65535 <ay65535@icloud.com>
2024-09-22 12:28:52 +0900

    C#だけの実装

--- CsbindingDotnetConsoleApp/Program.cs
+++ CsbindingDotnetConsoleApp/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace CsbindingDotnetConsoleApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CalcPoint(10000000);
+        }
+
+        static void CalcPoint(int points)
+        {
+            var src = new byte[points * 3];
+            var index = 0;
+
+            for (var i = 0; i < points; i++)
+            {
+                src[index++] = 1;
+                src[index++] = 2;
+                src[index++] = 3;
+            }
+
+            foreach (var i in Enumerable.Range(0, 9))
+            {
+                Console.WriteLine(src[i]);
+            }
         }
     }
 }
